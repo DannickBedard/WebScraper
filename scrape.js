2dfcabd90d11bfa241f68e99aaae328974bfe51a
Dannick <bedard.dannick@carrefour.cegepvicto.ca>
2020-09-13 17:02:46 -0400

    Début du dévelloppement pour le site ricardo

--- scrape.js
+++ scrape.js
@@ -1,17 +1,28 @@
 const request = require('request');
 const cheerio = require('cheerio');
 
-request('https://www.ricardocuisine.com/recettes/3856-pate-au-poulet', (error, responde, html) => {
-    if(!error && responde.statusCode == 200) {
-        const $ = cheerio.load(html);
-        
-        const test = $('.recipe-details-wrap');
+getInfoFromUrl('https://www.ricardocuisine.com/recettes/3856-pate-au-poulet');
 
-        console.log(test.html());
-
+function getInfoFromUrl(url) {
+    request(url, (error, responde, html) => {
+        if(!error && responde.statusCode == 200) {
+            const $ = cheerio.load(html);
+            
+            const ingredient = $('.ingredients');
+            const preparation = $('.preparation');
     
-    
-    }
+            const ingredientForm = ingredient.find('form');
 
-});
+            ingredientForm.find('ul').each((i, el) => {
+                if ($(el).prev().is('h3')) {
+                    console.log($(el).prev().text());
+                }
+                $(el).find('li').each((liIndex, liElement) => {
+                    console.log($(liElement).text());
+                });
+            });
+        }
+    });
+}
+
 
