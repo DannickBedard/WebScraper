c876e3ebe4304fd70bbd32a98ac0b21dcf36433f
Dannick <bedard.dannick@carrefour.cegepvicto.ca>
2020-09-26 16:19:07 -0400

    Tester le fonctionnement et inclure mongoDb

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
 
-const ricardo = require('./modules/ricardo.js');
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://dbMyRecipesUser:qTHHqIdB04zyirdF@clustermyrecipes.fna4m.mongodb.net/dbMyRecipes?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("dbMyRecipes").collection("MyRecipesCollection");
+  // perform actions on the collection object
+  console.log("Connecter à ma base de données");
+  console.log(collection);
+  client.close();
+});
 
-var  test = ricardo.get('https://www.ricardocuisine.com/recettes/3856-pate-au-poulet').then(client => { console.log(client) });;
 
-console.log(test);
+// const ricardo = require('./modules/ricardo.js');
+
+
+
+// var  ingredients =  ricardo.getIngredients('https://www.ricardocuisine.com/recettes/3856-pate-au-poulet');
+// var  preparations = ricardo.getPreparations('https://www.ricardocuisine.com/recettes/3856-pate-au-poulet');
+
+// console.log(ingredients);
+// console.log(preparations);
--- modules/ricardo.js
+++ modules/ricardo.js
@@ -1,12 +1,25 @@
-const request = require( '../node_modules/request');
-const cheerio = require( '../node_modules/cheerio');
 
-exports.get = async function getRecipe(url) {
+
+const request = require('../node_modules/request');
+const cheerio = require('../node_modules/cheerio');
+
+const MongoClient = require('../mongodb').MongoClient;
+const uri = "mongodb+srv://db_dannickbedard:<password>@cluster0.p8ox2.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log("Connecter to DB");
+  console.log(collection);
+  // perform actions on the collection object
+  client.close();
+});
+
+exports.getIngredients = async function getIngredients(url) {
     var ingredients = [];
     var preparations = [];
-     await request(url, (error, responde, html) => {
-         if(!error && responde.statusCode == 200) {
-            const $ = cheerio.load(html);
+    request(url, (error, responde, html) => {
+        if(!error && responde.statusCode == 200) {
+           const $ = cheerio.load(html);
             
             const ingredient = $('.ingredients');
             const preparation = $('.preparation');
@@ -14,28 +27,61 @@ exports.get = async function getRecipe(url) {
             const ingredientForm = ingredient.find('form');
 
             
+              const ingre =  getInfo($, ingredientForm).then(function(resp){
+                return resp
+              });
 
-            ingredientForm.find('ul, ol').each((i, el) => {
-                if ($(el).prev().is('h3')) {
-                    ingredients.push(["", $(el).prev().text().trim()]);
-                }
-                $(el).find('li').each((liIndex, liElement) => {
-                    ingredients.push([$(liElement).text().trim(), ""]);
-                });
-            });
+              return ingre;
 
-          
-            preparation.find('ul, ol').each((i, element) => {
-                if ($(element).prev().is('h3')) {
-                    preparations.push(["", $(element).prev().text().trim()])
-                }
-                $(element).find('li').each((liIndex, liElement) => {
-                    preparations.push([$(liElement).text().trim(), ""]);
-                });
-
-            });
+         
         }
-         return  {ingredients, preparations};
     });
 }
+
+
+exports.getPreparations = async function getPreparation(url) {
+    var ingredients = [];
+    var preparations = [];
+    request(url, (error, responde, html) => {
+        if(!error && responde.statusCode == 200) {
+           const $ = cheerio.load(html);
+            
+            const ingredient = $('.ingredients');
+            const preparation = $('.preparation');
+    
+            const ingredientForm = ingredient.find('form');
+
+              const prep =  getInfo($, preparation).then(function(resp){
+                return resp
+              });
+
+              console.log(prep);
+
+         
+        }
+    });
+}
+
+function getInfo($ ,element) {
+    let promise = new Promise(function(resolve, reject) {
+      var listeItem = [];
+      element.find('ul, ol').each((i, element) => {
+            if ($(element).prev().is('h3')) {
+                listeItem.push(["", $(element).prev().text().trim()])
+            }
+            $(element).find('li').each((liIndex, liElement) => {
+                listeItem.push([$(liElement).text().trim(), ""]);
+            });
+        });
+        if (listeItem.length >= 1){
+            resolve(listeItem);
+        }
+        else {
+            reject(false);
+        }
+    });
+    return promise;
+  }
+
+
 
